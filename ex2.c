b4cff9bc00b657690d494c1555baf6f55f56f0e9
Jessica Heitlauf <jrh2@hawaii.edu>
2021-06-23 13:50:37 -0700

    Yay I commited

---
+++ ex2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "ex2.h"
+
+void printJess()
+{
+    printf("Jess Contributed to this project\n");
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "ex1.h"
+#include "ex2.h"
 
 int main()
 {
     printBob();
+    printJess();
     return 0;
 }
